89334eed861e680773c658cea80a91c2e3419a50
Alejandro Daneri <alejandrodaneri07@gmail.com>
2020-10-20 16:44:02 -0300

    added medics table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import React, {useEffect, useState} from 'react'
+import CircleLoader from 'react-spinners/CircleLoader'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import axios from 'axios'
+import TableHead from "@material-ui/core/TableHead";
+
+
+function getVideos() {
+  return axios.get('https://cors-anywhere.herokuapp.com/https://cartillalibre-back.herokuapp.com/medics')
+}
+
 function App() {
+
+  const [medics, setMedics] = useState([1,2,3])
+
+  useEffect(() => {
+    getVideos().then(response => {
+      setMedics(response.data)
+    })
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Cartilla de medicos</h2>
+      {medics ? (
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Nombre</TableCell>
+              <TableCell>Especialidad</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {medics.length <= 0 ? (
+              <h3>No hay medicos</h3>
+            ) : (
+              medics.map(medic => {
+                return (
+                  <TableRow key={medic._id}>
+                    <TableCell>
+                          {medic.name}
+                    </TableCell>
+                    <TableCell>
+                          {medic.speciality}
+                    </TableCell>
+                  </TableRow>
+                )
+              })
+            )}
+          </TableBody>
+        </Table>
+      ) : (
+        <CircleLoader color={'blue'} size={250}/>
+      )}
     </div>
   );
 }
